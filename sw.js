2f7d654d4f2d653183b0c70a7a93d87ba08efa72
unknown <tomerpacific@gmail.com>
2019-05-04 09:51:50 +0300

    PWA adding logic to server worker to handle fetch requests

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
 var CACHE_NAME = 'androidmenugenerator';
+var STATUS_CODE_OK = 200;
+var BASIC_RESPONSE_TYPE = 'basic';
 var urlsToCache = [
 	'./',
    './index.html',
@@ -17,4 +19,31 @@ self.addEventListener('install', function(e) {
    	console.log("Service Worker: " + error);
    })
   ); 
+});
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+        .then(function(response){
+            if (!response || response.status !== STATUS_CODE_OK || response.type !== BASIC_RESPONSE_TYPE) {
+               return response;
+            }
+
+            var cachedResponse = response.clone();
+            caches.open(CACHE_NAME)
+            .then(function(cache) {
+               cache.put(event.request, cachedResponse);
+            });
+
+            return response;
+        });
+      }
+    )
+  );
 });
